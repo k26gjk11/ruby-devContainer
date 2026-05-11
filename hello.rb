7c14cfc2560d823cf448143bbc55fe9c1b5cc73c
Kiri NISHIHARA <145627772+k26gjk11@users.noreply.github.com>
2026-05-11 12:41:09 +0900

    変数と条件分岐と繰り返し

--- hello.rb
+++ hello.rb
@@ -1,3 +1,28 @@
 #! /usr/bin/env ruby
 
 puts "Hello, World!"
+a=5
+s="Ruby"
+d=3.14
+puts "a:"+a.to_s()
+puts "s:"+s
+puts "d:"+d.to_s()
+if a>5
+    puts "Large"
+else 
+    if a<0
+    puts "Negative"
+else
+    puts"Little"
+end
+end
+x=5
+while x>0
+    print "#"
+    x-=1
+end
+puts
+S.times do[i]
+    print i.to_s
+end
+puts
