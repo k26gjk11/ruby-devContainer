936f41ffba0d230286565281f7f90b38944cd17f
Kiri NISHIHARA <145627772+k26gjk11@users.noreply.github.com>
2026-06-15 12:33:58 +0900

    HTTP/1.0 ヘッダーを表示しない

--- client1.rb
+++ client1.rb
@@ -5,13 +5,16 @@ require 'socket'
 
 host = 'www.is.kyusan-u.ac.jp'
 port = 'http'
-path = '/~toshi~'
+path = '/~toshi/'
 
 sock = TCPSocket.new host, port
 cmd = 'GET ' + path + " HTTP/1.0\r\n\r\n"
 pp cmd
 sock.print cmd
 
+is_body = false
 while line = sock.gets
-  puts line
+#  pp line
+  puts line if is_body
+  is_body = true if line == "\r\n"
 end
