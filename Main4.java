7e168b156020195567107c436ab31674d43a7f14
Kiri NISHIHARA <145627772+k26gjk11@users.noreply.github.com>
2026-06-22 12:01:34 +0900

    1.1に対応

---
+++ Main4.java
@@ -0,0 +1,10 @@
+class Main4 {
+    public static void main(String[] args) {
+        String a = "Hello";
+        char b = '0';
+        System.out.println(a);
+        System.out.println(b);
+        char c = (char)(b+3);
+        System.out.println(c);
+    }
+}
--- client1.rb
+++ client1.rb
@@ -8,7 +8,7 @@ port = 'http'
 path = '/~toshi/'
 
 sock = TCPSocket.new host, port
-cmd = 'GET ' + path + " HTTP/1.0\r\n\r\n"
+cmd = 'GET ' + path + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n"
 pp cmd
 sock.print cmd
 
