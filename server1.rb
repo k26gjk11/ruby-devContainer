26bdbe2246ff862aa22dc555f672eb8667092500
Kiri NISHIHARA <145627772+k26gjk11@users.noreply.github.com>
2026-06-29 12:03:48 +0900

    create serve1
    とりあえず動く

--- server1.rb
+++ server1.rb
@@ -4,8 +4,14 @@
 require 'socket'
 
 gs = TCPServer.open 'http'
-s = gs.accept
-while line = s.gets
-  pp line
-  s.puts line
-end
+loop do
+  pp "start accept"
+  s = gs.accept
+  while line = s.gets
+    pp line
+    s.puts line
+    break if line == "\r\n"
+  end
+    s.close
+    sleep 10
+  end
