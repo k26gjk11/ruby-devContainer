0936aa763e4d6a9757cd5db9003bebd29e9bcae1
Kiri NISHIHARA <145627772+k26gjk11@users.noreply.github.com>
2026-07-06 11:40:40 +0900

    PATHを一応見る

--- server1.rb
+++ server1.rb
@@ -4,11 +4,16 @@
 require 'socket'
 
 def server s
-  while line = s.gets
-    pp line
-    s.puts line
-    break if line == "\r\n"
+  cmd, path, ver = s.gets.split " "
+  if path == "/"
+    pp "INDEX"
+    s.puts "index"
+  else
+    pp "OTHER"
+    s.puts "other"
   end
+  s.close
+end
   s.close
 end
 
